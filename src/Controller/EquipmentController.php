54fb89e296bbdc623601274b5be1651cb3b73503
Elvis Caceres <edcalo@gmail.com>
2013-03-20 22:41:30 -0400

    Add netbeans project files

--- src/Controller/EquipmentController.php
+++ src/Controller/EquipmentController.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppController', 'Controller');
+
 /**
  * Equipment Controller
  *
@@ -7,195 +9,199 @@ App::uses('AppController', 'Controller');
  */
 class EquipmentController extends AppController {
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->Equipment->recursive = 0;
-		$this->set('equipment', $this->paginate());
-	}
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index() {
+        $this->Equipment->recursive = 0;
+        $this->set('equipment', $this->paginate());
+    }
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		$this->Equipment->id = $id;
-		if (!$this->Equipment->exists()) {
-			throw new NotFoundException(__('Invalid equipment'));
-		}
-		$this->set('equipment', $this->Equipment->read(null, $id));
-	}
+    /**
+     * view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function view($id = null) {
+        $this->Equipment->id = $id;
+        if (!$this->Equipment->exists()) {
+            throw new NotFoundException(__('Invalid equipment'));
+        }
+        $this->set('equipment', $this->Equipment->read(null, $id));
+        $statuses = $this->Equipment->Status->find('list');
+        $pieces = $this->Equipment->Item->Piece->find('list');
+        $this->set(compact('statuses', 'pieces'));
+    }
 
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Equipment->create();
-			if ($this->Equipment->save($this->request->data)) {
-				$this->Session->setFlash(__('The equipment has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The equipment could not be saved. Please, try again.'));
-			}
-		}
-		$statuses = $this->Equipment->Status->find('list');
-		$categories = $this->Equipment->Category->find('list');
-		$this->set(compact('statuses', 'categories'));
-	}
+    /**
+     * add method
+     *
+     * @return void
+     */
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Equipment->create();
+            if ($this->Equipment->save($this->request->data)) {
+                $this->Session->setFlash(__('The equipment has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The equipment could not be saved. Please, try again.'));
+            }
+        }
+        $statuses = $this->Equipment->Status->find('list');
+        $categories = $this->Equipment->Category->find('list');
+        $this->set(compact('statuses', 'categories'));
+    }
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		$this->Equipment->id = $id;
-		if (!$this->Equipment->exists()) {
-			throw new NotFoundException(__('Invalid equipment'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Equipment->save($this->request->data)) {
-				$this->Session->setFlash(__('The equipment has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The equipment could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->Equipment->read(null, $id);
-		}
-		$statuses = $this->Equipment->Status->find('list');
-		$categories = $this->Equipment->Category->find('list');
-		$this->set(compact('statuses', 'categories'));
-	}
+    /**
+     * edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function edit($id = null) {
+        $this->Equipment->id = $id;
+        if (!$this->Equipment->exists()) {
+            throw new NotFoundException(__('Invalid equipment'));
+        }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->Equipment->save($this->request->data)) {
+                $this->Session->setFlash(__('The equipment has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The equipment could not be saved. Please, try again.'));
+            }
+        } else {
+            $this->request->data = $this->Equipment->read(null, $id);
+        }
+        $statuses = $this->Equipment->Status->find('list');
+        $categories = $this->Equipment->Category->find('list');
+        $this->set(compact('statuses', 'categories'));
+    }
 
-/**
- * delete method
- *
- * @throws MethodNotAllowedException
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Equipment->id = $id;
-		if (!$this->Equipment->exists()) {
-			throw new NotFoundException(__('Invalid equipment'));
-		}
-		if ($this->Equipment->delete()) {
-			$this->Session->setFlash(__('Equipment deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Equipment was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
+    /**
+     * delete method
+     *
+     * @throws MethodNotAllowedException
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function delete($id = null) {
+        if (!$this->request->is('post')) {
+            throw new MethodNotAllowedException();
+        }
+        $this->Equipment->id = $id;
+        if (!$this->Equipment->exists()) {
+            throw new NotFoundException(__('Invalid equipment'));
+        }
+        if ($this->Equipment->delete()) {
+            $this->Session->setFlash(__('Equipment deleted'));
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Equipment was not deleted'));
+        $this->redirect(array('action' => 'index'));
+    }
 
-/**
- * admin_index method
- *
- * @return void
- */
-	public function admin_index() {
-		$this->Equipment->recursive = 0;
-		$this->set('equipment', $this->paginate());
-	}
+    /**
+     * admin_index method
+     *
+     * @return void
+     */
+    public function admin_index() {
+        $this->Equipment->recursive = 0;
+        $this->set('equipment', $this->paginate());
+    }
 
-/**
- * admin_view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function admin_view($id = null) {
-		$this->Equipment->id = $id;
-		if (!$this->Equipment->exists()) {
-			throw new NotFoundException(__('Invalid equipment'));
-		}
-		$this->set('equipment', $this->Equipment->read(null, $id));
-	}
+    /**
+     * admin_view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function admin_view($id = null) {
+        $this->Equipment->id = $id;
+        if (!$this->Equipment->exists()) {
+            throw new NotFoundException(__('Invalid equipment'));
+        }
+        $this->set('equipment', $this->Equipment->read(null, $id));
+    }
 
-/**
- * admin_add method
- *
- * @return void
- */
-	public function admin_add() {
-		if ($this->request->is('post')) {
-			$this->Equipment->create();
-			if ($this->Equipment->save($this->request->data)) {
-				$this->Session->setFlash(__('The equipment has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The equipment could not be saved. Please, try again.'));
-			}
-		}
-		$statuses = $this->Equipment->Status->find('list');
-		$categories = $this->Equipment->Category->find('list');
-		$this->set(compact('statuses', 'categories'));
-	}
+    /**
+     * admin_add method
+     *
+     * @return void
+     */
+    public function admin_add() {
+        if ($this->request->is('post')) {
+            $this->Equipment->create();
+            if ($this->Equipment->save($this->request->data)) {
+                $this->Session->setFlash(__('The equipment has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The equipment could not be saved. Please, try again.'));
+            }
+        }
+        $statuses = $this->Equipment->Status->find('list');
+        $categories = $this->Equipment->Category->find('list');
+        $this->set(compact('statuses', 'categories'));
+    }
 
-/**
- * admin_edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function admin_edit($id = null) {
-		$this->Equipment->id = $id;
-		if (!$this->Equipment->exists()) {
-			throw new NotFoundException(__('Invalid equipment'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Equipment->save($this->request->data)) {
-				$this->Session->setFlash(__('The equipment has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The equipment could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->Equipment->read(null, $id);
-		}
-		$statuses = $this->Equipment->Status->find('list');
-		$categories = $this->Equipment->Category->find('list');
-		$this->set(compact('statuses', 'categories'));
-	}
+    /**
+     * admin_edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function admin_edit($id = null) {
+        $this->Equipment->id = $id;
+        if (!$this->Equipment->exists()) {
+            throw new NotFoundException(__('Invalid equipment'));
+        }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->Equipment->save($this->request->data)) {
+                $this->Session->setFlash(__('The equipment has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The equipment could not be saved. Please, try again.'));
+            }
+        } else {
+            $this->request->data = $this->Equipment->read(null, $id);
+        }
+        $statuses = $this->Equipment->Status->find('list');
+        $categories = $this->Equipment->Category->find('list');
+        $this->set(compact('statuses', 'categories'));
+    }
 
-/**
- * admin_delete method
- *
- * @throws MethodNotAllowedException
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function admin_delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Equipment->id = $id;
-		if (!$this->Equipment->exists()) {
-			throw new NotFoundException(__('Invalid equipment'));
-		}
-		if ($this->Equipment->delete()) {
-			$this->Session->setFlash(__('Equipment deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Equipment was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
+    /**
+     * admin_delete method
+     *
+     * @throws MethodNotAllowedException
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function admin_delete($id = null) {
+        if (!$this->request->is('post')) {
+            throw new MethodNotAllowedException();
+        }
+        $this->Equipment->id = $id;
+        if (!$this->Equipment->exists()) {
+            throw new NotFoundException(__('Invalid equipment'));
+        }
+        if ($this->Equipment->delete()) {
+            $this->Session->setFlash(__('Equipment deleted'));
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Equipment was not deleted'));
+        $this->redirect(array('action' => 'index'));
+    }
+
 }
--- src/Model/Piece.php
+++ src/Model/Piece.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppModel', 'Model');
+
 /**
  * Piece Model
  *
@@ -9,127 +11,130 @@ App::uses('AppModel', 'Model');
  */
 class Piece extends AppModel {
 
-/**
- * Use database config
- *
- * @var string
- */
-	public $useDbConfig = 'inventory';
+    /**
+     * Use database config
+     *
+     * @var string
+     */
+    public $useDbConfig = 'inventory';
 
-/**
- * Display field
- *
- * @var string
- */
-	public $displayField = 'piece_name';
+    /**
+     * Display field
+     *
+     * @var string
+     */
+    public $displayField = 'full_name';
+    
+    public $virtualFields = array("full_name"=>"CONCAT(piece_name, ' ' ,model)");
+   
 
-/**
- * Validation rules
- *
- * @var array
- */
-	public $validate = array(
-		'manufacturer_id' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'piece_name' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'model' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'is_internal_piece' => array(
-			'boolean' => array(
-				'rule' => array('boolean'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'is_delete' => array(
-			'boolean' => array(
-				'rule' => array('boolean'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public $validate = array(
+        'manufacturer_id' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            //'message' => 'Your custom message here',
+            //'allowEmpty' => false,
+            //'required' => false,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'piece_name' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            //'message' => 'Your custom message here',
+            //'allowEmpty' => false,
+            //'required' => false,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'model' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            //'message' => 'Your custom message here',
+            //'allowEmpty' => false,
+            //'required' => false,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'is_internal_piece' => array(
+            'boolean' => array(
+                'rule' => array('boolean'),
+            //'message' => 'Your custom message here',
+            //'allowEmpty' => false,
+            //'required' => false,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'is_delete' => array(
+            'boolean' => array(
+                'rule' => array('boolean'),
+            //'message' => 'Your custom message here',
+            //'allowEmpty' => false,
+            //'required' => false,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+    );
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
 
-/**
- * belongsTo associations
- *
- * @var array
- */
-	public $belongsTo = array(
-		'Manufacturer' => array(
-			'className' => 'Manufacturer',
-			'foreignKey' => 'manufacturer_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
+    /**
+     * belongsTo associations
+     *
+     * @var array
+     */
+    public $belongsTo = array(
+        'Manufacturer' => array(
+            'className' => 'Manufacturer',
+            'foreignKey' => 'manufacturer_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
 
-/**
- * hasMany associations
- *
- * @var array
- */
-	public $hasMany = array(
-		'Feature' => array(
-			'className' => 'Feature',
-			'foreignKey' => 'piece_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		),
-		'Item' => array(
-			'className' => 'Item',
-			'foreignKey' => 'piece_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
+    /**
+     * hasMany associations
+     *
+     * @var array
+     */
+    public $hasMany = array(
+        'Feature' => array(
+            'className' => 'Feature',
+            'foreignKey' => 'piece_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        ),
+        'Item' => array(
+            'className' => 'Item',
+            'foreignKey' => 'piece_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        )
+    );
 
 }
